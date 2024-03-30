4305b93487f3eef683c2f3c3966a6e89c972e248
pqrsuyash <pqrsuyash@gmail.com>
2024-03-30 12:25:35 +0530

    commit

---
+++ day 4/24_arrays.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+vector<int> arr={1,2,3};
+cout<<endl;
+for(int i=0;i<arr.size();i++){
+    cout<<arr[i]<<endl;
+}
+arr.pop_back();
+for(int i=0;i<arr.size();i++){
+    cout<<arr[i]<<endl;
+}
+ return 0;
+}
