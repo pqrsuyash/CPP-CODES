bbca344b6b9533b864dffe2a809c8385d58e6983
pqrsuyash <pqrsuyash@gmail.com>
2024-04-06 22:05:03 +0530

    commit

---
+++ day 9/44_pointers.cpp
@@ -0,0 +1,19 @@
+//practice pointers
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //arr[i]=*(arr+i)
+    //i[arr]=arr[i]
+
+    //cout implementation in case of integer arrays and string is different
+int arr[10]={1,2,3};
+int *ptr=&arr[0];
+
+cout<<1[arr];
+
+ return 0;
+}
